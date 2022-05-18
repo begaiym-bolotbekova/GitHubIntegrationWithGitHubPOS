f00c28b9eb1956bbfd01227b5bfd1417dc4cb9c8
Begaiym Bolotbekova <begaiym.bolotbekova@gmail.com>
2022-05-18 19:21:38 +0200

    Zeile 3 wurde hinzugefügt!

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
         System.out.println("Zeile 3");
+        System.out.println("Zeile 4");
 
     }
 }
